a9a9460c0eaec3cc91082b403045c1161e4413b7
death <deathangel908@gmail.com>
2025-11-23 22:57:06 +0100

    Removed more dependencies

--- src/config/fields-validator.ts
+++ src/config/fields-validator.ts
@@ -1,12 +1,13 @@
 import {Inject, Injectable} from '@nestjs/common';
-import {INativeModule, Native} from "@/native/native-model";
+import {INativeModule, Native, NativeCameraInfo} from "@/native/native-model";
 import {Telegraf} from "telegraf";
 import {Platform, TelegrafGet} from "@/config/config-resolve-model";
 
 @Injectable()
 export class FieldsValidator {
 
-  private tGToken: string|null = null;
+  private tGToken: string | null = null;
+  private cameras: NativeCameraInfo[] | null = null;
 
   constructor(
     @Inject(Native)
@@ -19,18 +20,23 @@ export class FieldsValidator {
   }
 
   public validateCamera(value: any) {
-    const cameras = this.native.listAvailableCameras();
-    if (cameras.find(c => c.path === value)) {
-      return true;
+    if (!this.cameras) {
+      this.cameras = this.native.listAvailableCameras();
     }
-    let s = '';
-    if (this.platform === 'linux') {
-      s = cameras.map(c => `${c.path} => ${c.name}`).join('\n');
-    } else {
-      s = cameras.map(c => c.name).join('\n');
+    if (this.platform === 'win32') {
+      if (this.cameras.find(c => c.name === value)) {
+        return true;
+      }
+      const cmrStr = this.cameras.map(c => c.name).join('\n');
+      return `Invalid camera name, available devices: \n ${cmrStr}`;
+    } else if (this.platform === 'linux') {
+      if (this.cameras.find(c => c.path === value)) {
+        return true;
+      }
+      const cmrStr = this.cameras.map(c => `${c.path} => ${c.name}`).join('\n');
+      return `Invalid camera name, available devices: \n ${cmrStr}`;
     }
-
-    return `Invalid camera name, available devices: \n ${s}`
+    return true;
   }
 
   public async validateTgChat(value: any) {
@@ -40,7 +46,7 @@ export class FieldsValidator {
     const bot = this.getTG(this.tGToken!)
     const tokenValid = await bot.telegram.getChat(value).then(() => true).catch(() => false);
     if (!tokenValid) {
-      return 'Invalid Chat id';
+      return 'ChatID not found';
     }
     return true;
   }
